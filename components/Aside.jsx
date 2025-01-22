ee4fb0e994b3217e76b38087848d696803c6c4ea
abnakore <abdullahinakore@gmail.com>
2025-01-22 14:18:07 +0100

    Updated Aside Component

--- components/Aside.jsx
+++ components/Aside.jsx
@@ -80,48 +80,7 @@ return (
           )}
         </ul>
 
-        {/* <Link to='/' className="tab">Vote Page</Link>
-                    <Link to='/result' className="tab">Results</Link>
-                    <Link to='/' className="tab">Login/Logout</Link> */}
       </div>
     </div>
   </Wrapper>
 );
-//
-
-// .aside {
-//   position: fixed;
-//   background-color: #333;
-//   padding: 20px;
-//   height: 50px;
-//   width: 100vw;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: space-between;
-//   top: 0;
-//   left: 0;
-//   z-index: 100;
-// }
-// .aside #tabs {
-//   display: flex;
-//   flex-direction: row;
-//   gap: 20px;
-// }
-// .aside #tabs .tab {
-//   padding: 10px;
-//   font-size: 16px;
-//   color: #fff;
-//   text-align: center;
-//   cursor: pointer;
-//   transition: background-color 0.3s ease;
-// }
-
-// .aside #tabs .tab:hover, .aside #tabs .active {
-//   background-color: #555;
-// }
-
-// <a href="#">Home</a>
-// <a href="#">About</a>
-// <a href="#">Services</a>
-// <a href="#">Contact</a>
