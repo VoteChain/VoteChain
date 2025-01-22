75bdb1454bbc127bd57f699e5431b9b9e537e9f8
abnakore <abdullahinakore@gmail.com>
2025-01-22 14:25:20 +0100

    Updated Input

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,32 +1,32 @@
 const Wrapper = styled.div`
-label {
-  position: relative;
-}
-label .input {
-  background-color: #333;
-  color: #fff;
-  width: 100%;
-  padding: 20px 5px 5px 10px;
-  outline: 0;
-  border: 1px solid rgba(105, 105, 105, 0.397);
-  border-radius: 10px;
-  font-size: medium;
-}
+  label {
+    position: relative;
+  }
+  label .input {
+    background-color: #333;
+    color: #fff;
+    width: 100%;
+    padding: 20px 5px 5px 10px;
+    outline: 0;
+    border: 1px solid rgba(105, 105, 105, 0.397);
+    border-radius: 10px;
+    font-size: medium;
+  }
 
-.input + span {
-  color: rgba(255, 255, 255, 0.5);
-  position: absolute;
-  left: 10px;
-  top: 0px;
-  font-size: 0.9em;
-  cursor: text;
-  transition: 0.3s ease;
-}
+  .input + span {
+    color: rgba(255, 255, 255, 0.5);
+    position: absolute;
+    left: 10px;
+    top: 0px;
+    font-size: 0.9em;
+    cursor: text;
+    transition: 0.3s ease;
+  }
 
-.input:placeholder-shown + span {
-  top: 12.5px;
-  font-size: 0.9em;
-}
+  .input:placeholder-shown + span {
+    top: 12.5px;
+    font-size: 0.9em;
+  }
 `;
 const itemIndex = props.item;
 console.log(`item = ${itemIndex}`);
