ad601f8dce544a4e71ad71ab24a666b73a5b2725
abnakore <abdullahinakore@gmail.com>
2025-01-22 14:23:54 +0100

    Updated Vote chain home page

--- components/VoteChain.jsx
+++ components/VoteChain.jsx
@@ -2,7 +2,9 @@
 const accountId = context.accountId;
 
 // All the votes
-const [allVotes, setAllVotes] = useState([]);
+// Get all the votes
+const allVotes = Social.index("voteChainTest", "vote");
+const otherCandidates = Social.index("voteChainTest", "candidate");
 
 // Tabs
 const [tab, setTab] = useState(props.tab ? props.tab : "all");
@@ -13,23 +15,13 @@ const [watchlist, setWatchlist] = useState([]);
 // Votes to be rendered on the screen
 const [votesToRender, setVotesToRender] = useState(allVotes);
 
-// Get all the votes
-const votesData = Social.get(`abnakore.near/votes`);
-
-useEffect(() => {
-  //   console.log(votesData);
-  if (votesData === undefined) {
-    // Set the candidate to an empty list if there is no candidate
-    setAllVotes([]);
-  } else {
-    setAllVotes(JSON.parse(votesData));
-  }
-  setVotesToRender(allVotes);
-  //   console.log(votesData, "");
-}, [votesData === null]);
-
 // Get The watchlist of the user
 const watchlistData = Social.get(`${accountId}/voteChain_watchlist`);
+
+useEffect(() => {
+  setVotesToRender(allVotes);
+  console.log(votesToRender, allVotes, "votesData");
+}, [allVotes]);
 
 useEffect(() => {
   if (watchlistData === undefined) {
@@ -41,35 +33,38 @@ useEffect(() => {
 }, [watchlistData === null]);
 
 useEffect(() => {
-  console.log(tab, allVotes);
   // Get the only votes the user created
   if (tab === "my_votes") {
-    setVotesToRender(allVotes.filter((vote) => vote.creator === accountId));
+    setVotesToRender(
+      allVotes.filter((vote) => vote.value.creator === accountId)
+    );
   } else if (tab === "watchlist") {
-    setVotesToRender(allVotes.filter((vote) => watchlist.includes(vote.id)));
+    setVotesToRender(
+      allVotes.filter((vote) => watchlist.includes(vote.blockHeight))
+    );
   } else {
     setVotesToRender(allVotes);
   }
-}, [allVotes, tab, watchlist]);
+}, [watchlist]);
 
 // Pages that can be reached via the aside tab
 const [pages, setPage] = useState([
   {
     name: "All Votes",
-    link: "https://near.org/abnakore.near/widget/VoteChain",
+    link: "https://near.social/abnakore.near/widget/VoteChain",
   },
   {
     name: "My Votes",
-    link: "https://near.org/abnakore.near/widget/VoteChain?tab=my_votes",
+    link: "https://near.social/abnakore.near/widget/VoteChain?tab=my_votes",
   },
   {
     name: "Watchlist",
-    link: "https://near.org/abnakore.near/widget/VoteChain?tab=watchlist",
+    link: "https://near.social/abnakore.near/widget/VoteChain?tab=watchlist",
   },
   {
     name: "Create New Vote",
     type: "button",
-    link: "https://near.org/abnakore.near/widget/CreateVote",
+    link: "https://near.social/abnakore.near/widget/CreateVote",
   },
 ]);
 
@@ -92,13 +87,13 @@ function formatDateTime(dateTimeString) {
 }
 
 const List = styled.div`
-width: calc(100vw - 20px);
-height: 100%;
-padding: 20px 10px;
+  width: calc(100vw - 20px);
+  height: 100%;
+  padding: 20px 10px;
 
-@media screen and (min-width: 1200px) {
-  width: calc(100vw - 20%);
-}
+  @media screen and (min-width: 1200px) {
+    width: calc(100vw - 20%);
+  }
 `;
 return (
   <>
@@ -109,7 +104,8 @@ return (
           body: (
             <div className="main-body">
               <div className="body-contents">
-                <div className="two-sides">
+                <Widget src="abnakore.near/widget/Hero" props={{}} />
+                <div id="votes" className="two-sides">
                   <Widget
                     src="abnakore.near/widget/Aside"
                     props={{ objs: pages, active: "/admin/manage_candidates" }}
@@ -118,18 +114,19 @@ return (
                     <List>
                       {votesToRender.map((vote) => (
                         <a
-                          href={`https://near.org/abnakore.near/widget/App.jsx?vote=${vote.id}`}
+                          href={`https://near.social/abnakore.near/widget/App.jsx?vote=${vote.blockHeight}`}
                         >
                           <Widget
                             src="abnakore.near/widget/VoteCard"
                             props={{
-                              name: vote.name,
-                              desc: vote.desc,
-                              open: formatDateTime(vote.openTime),
-                              close: formatDateTime(vote.closeTime),
-                              voted: vote.voters.includes(accountId),
-                              locked: vote.passcode && true,
-                              no_of_candidates: vote.candidates.length,
+                              ...vote.value,
+                              candidates: vote.value.candidates.concat(
+                                otherCandidates.filter(
+                                  (candidate) =>
+                                    parseFloat(candidate.value.voteId) ===
+                                    parseFloat(vote.blockHeight)
+                                )
+                              ),
                               style: {},
                             }}
                           />
@@ -152,75 +149,3 @@ return (
     )}
   </>
 );
-
-// // Get accountId
-// const accountId = context.accountId;
-
-// // Pages that can be reached via the aside tab
-// const [pages, setPage] = useState([
-//   {
-//     name: "Home",
-//     link: "https://near.org/sandbox#/abnakore.near/widget/AdminHome",
-//   },
-//   {
-//     name: "Manage Candidates",
-//     link: "https://near.org/sandbox#/abnakore.near/widget/ManageCandidates",
-//   },
-//   {
-//     name: "Mange Parties",
-//     link: "https://near.org/sandbox#/abnakore.near/widget/ManageParties",
-//   },
-// ]);
-// return (
-//   <>
-//     {accountId ? (
-//       <Widget
-//         src="abnakore.near/widget/Wrapper"
-//         props={{
-//           body: (
-//             <div className="main-body">
-//               <div className="body-contents">
-//                 <div className="two-sides"></div>
-//                 <Widget
-//                   src="abnakore.near/widget/Aside"
-//                   props={{ objs: pages, active: "/admin/manage_candidates" }}
-//                 />
-//                 <div className="body-contents">
-//                   <h1>...</h1>
-//                 </div>
-//               </div>
-//             </div>
-//           ),
-//         }}
-//       />
-//     ) : (
-//       <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
-//     )}
-//   </>
-// );
-
-//
-// <Widget
-//   src="abnakore.near/widget/VoteCard"
-//   props={{
-//     name: "Name",
-//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit obcaecati iure rem nulla deleniti accusamus, qui, aut recusandae ut fugiat molestiae dicta asperiores, est aspernatur. Aliquid porro sed neque molestiae!",
-//     open: "27-03-2024 2:30PM",
-//     close: "27-03-2024 2:30PM",
-//     no_of_candidates: 10,
-//     voted: true,
-//     locked: false,
-//   }}
-// />
-// <Widget
-//   src="abnakore.near/widget/VoteCard"
-//   props={{
-//     name: "Name",
-//     desc: "You can use the slice method along with the spread operator (...) to create a new array containing the first two items.",
-//     open: "27-03-2024 2:30PM",
-//     close: "27-03-2024 2:30PM",
-//     no_of_candidates: 10,
-//     voted: false,
-//     locked: true,
-//   }}
-// />
