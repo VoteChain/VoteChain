e4ac71c5ff171a4945da207a97e4c25be6b3b1f6
abnakore <abdullahinakore@gmail.com>
2025-01-30 23:49:27 +0100

    updated Home page

--- components/VoteChain.jsx
+++ components/VoteChain.jsx
@@ -191,10 +191,6 @@ const Wrapper = styled.div`
 return (
   <>
     {accountId ? (
-      //   <Widget
-      //     src="abnakore.near/widget/Wrapper"
-      //     props={{
-      //       body: (
       <Wrapper>
         <div className="main-body">
           <div className="body-contents">
@@ -244,82 +240,7 @@ return (
         </div>
       </Wrapper>
     ) : (
-      //       ),
-      //     }}
-      //   />
       <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
     )}
   </>
 );
-
-// // Get accountId
-// const accountId = context.accountId;
-
-// // Pages that can be reached via the aside tab
-// const [pages, setPage] = useState([
-//   {
-//     name: "Home",
-//     link: "https://near.social/sandbox#/abnakore.near/widget/AdminHome",
-//   },
-//   {
-//     name: "Manage Candidates",
-//     link: "https://near.social/sandbox#/abnakore.near/widget/ManageCandidates",
-//   },
-//   {
-//     name: "Mange Parties",
-//     link: "https://near.social/sandbox#/abnakore.near/widget/ManageParties",
-//   },
-// ]);
-// return (
-//   <>
-//     {accountId ? (
-//       <Widget
-//         src="abnakore.near/widget/Wrapper"
-//         props={{
-//           body: (
-//             <div className="main-body">
-//               <div className="body-contents">
-//                 <div className="two-sides"></div>
-//                 <Widget
-//                   src="abnakore.near/widget/Aside"
-//                   props={{ objs: pages, active: "/admin/manage_candidates" }}
-//                 />
-//                 <div className="body-contents">
-//                   <h1>...</h1>
-//                 </div>
-//               </div>
-//             </div>
-//           ),
-//         }}
-//       />
-//     ) : (
-//       <Widget src="abnakore.near/widget/SignIn.jsx" props={props} />
-//     )}
-//   </>
-// );
-
-//
-// <Widget
-//   src="abnakore.near/widget/VoteCard"
-//   props={{
-//     name: "Name",
-//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit obcaecati iure rem nulla deleniti accusamus, qui, aut recusandae ut fugiat molestiae dicta asperiores, est aspernatur. Aliquid porro sed neque molestiae!",
-//     open: "27-03-2024 2:30PM",
-//     close: "27-03-2024 2:30PM",
-//     no_of_candidates: 10,
-//     voted: true,
-//     locked: false,
-//   }}
-// />
-// <Widget
-//   src="abnakore.near/widget/VoteCard"
-//   props={{
-//     name: "Name",
-//     desc: "You can use the slice method along with the spread operator (...) to create a new array containing the first two items.",
-//     open: "27-03-2024 2:30PM",
-//     close: "27-03-2024 2:30PM",
-//     no_of_candidates: 10,
-//     voted: false,
-//     locked: true,
-//   }}
-// />
