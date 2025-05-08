61e65bcf2b99b25a9295b1c0ad7c9261d0b6f262
abnakore <abdullahinakore@gmail.com>
2025-05-08 17:52:39 +0100

    Added Hero container and aside and button components

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,18 +11,16 @@ function App() {
         <a href="/" target="_blank">
           <img src={"/images/logo.jpg"} className="logo" alt="Vite logo" />
         </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img
-            src={"/images/logo.jpg"}
-            className="logo react"
-            alt="React logo"
-          />
-        </a> */}
       </div>
       <h1>VoteChain</h1>
       <p>A decentralized voting system.</p>
       <div className="card">
         <h1>Comming soon!!!</h1>
+        <Button
+          title={"Get Started"}
+          handleClick={() => console.log("Get started")}
+          theme={"primary"}
+        />
       </div>
     </>
   );
